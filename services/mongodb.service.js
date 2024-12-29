e7c45950ebeca46934d129d8a43fdaa4314f6abb
Kimrama <kimrama0x@gmail.com>
2024-12-29 12:54:46 +0700

    use local host machine - mongodb

--- services/mongodb.service.js
+++ services/mongodb.service.js
@@ -11,7 +11,9 @@ mongoose.connection.on("error", (err) => {
 async function mongodbConnect() {
     try {
         console.log("connecting to mongodb");
-        await mongoose.connect("mongodb://localhost:27017/swfavorites");
+        await mongoose.connect(
+            "mongodb://host.docker.internal:27017/swfavorites"
+        );
         console.log("mongodb is connected");
     } catch (error) {
         throw new Error("something went wrong connecting to mongodb");
